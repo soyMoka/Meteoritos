c6e995ae2be1af2c4d2d4c72ac4fcce4f0f1910e
soyMoka <d15.cejas.matias@gmail.com>
2022-10-11 05:15:34 -0300

    Disparos player agregados

--- Juego/Naves/Player.gd
+++ Juego/Naves/Player.gd
@@ -1,3 +1,4 @@
+class_name Player
 extends RigidBody2D
 
 
@@ -10,12 +11,12 @@ export var potencia_rotacion:int = 280
 var empuje:Vector2 = Vector2.ZERO
 var dir_rotacion:int = 0
 
+## Atributos Onready
+onready var canion:Canion = $Canion
+
 
 ## Metodos
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
-	# Prints para entender
-	print(rad2deg(rotation))
-	print(empuje.rotated(rotation))
 	
 	apply_central_impulse(empuje.rotated(rotation))
 	apply_torque_impulse(dir_rotacion * potencia_rotacion)
@@ -26,18 +27,26 @@ func _process(delta: float) -> void:
 
 ## Metodos Custom
 func player_input() -> void:
+	
 	# Empuje
 	empuje = Vector2.ZERO
 	if Input.is_action_pressed("mover_adelante"):
 		empuje = Vector2(potencia_motor, 0)
 	elif Input.is_action_pressed("mover_atras"):
 		empuje = Vector2(-potencia_motor, 0)
+	
 	# Rotacion
 	dir_rotacion = 0
 	if  Input.is_action_pressed("rotar_antihorario"):
 		dir_rotacion -=1
 	elif  Input.is_action_pressed("rotar_horario"):
 		dir_rotacion +=1
+	
+	# Disparo
+	if Input.is_action_pressed("disparo_principal"):
+		canion.set_esta_disparando(true)
+	if Input.is_action_just_released("disparo_principal"):
+		canion.set_esta_disparando(false)	
 		
 	
 	
